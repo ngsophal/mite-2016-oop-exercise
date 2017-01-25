1300e4dd463f9a6322c5b76b7f26a75751533b5f
Sophal Ngann <ngannsophal12@gmail.com>
2017-01-25 23:29:43 +0700

    convert oop exercise into new maven project

---
+++ src/main/java/edu/rupp/repo/Person.java
@@ -0,0 +1,11 @@
+package edu.rupp.repo;
+
+import java.util.Date;
+/*Create a Class “Person” extending class "Dcommon" with the following 
+properties: birthDate (Date), lastName (String), firstName (String)
+*/
+public class Person extends DCommon{
+	protected Date birthDate = new Date(); 
+	protected String lastName; 
+	protected String firstName;
+}
---
+++ src/main/java/edu/rupp/repo/View.java
@@ -0,0 +1,9 @@
+package edu.rupp.repo;
+/*Create Interface “View” with method getDataView() return as String.
+
+*/
+public interface View {
+	public String getDataView();
+}
+
+
